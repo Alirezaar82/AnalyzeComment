69d380cb5edaadb274a6d14ab568fba66a98a4bd
Alirezaar82 <153992724+Alirezaar82@users.noreply.github.com>
2024-11-09 19:27:29 +0330

    clean code

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ model = load_model('model.h5')
 def analyze_comment():
     sentence = entry.get() 
     this_sentence = sentence.lower().split(' ')
-    print(this_sentence)
     
     try:
        
